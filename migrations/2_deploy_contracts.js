a4cca82eb51870e1ba9f2e6c4e6a5ae5744986ef
dev.koold <dev.koold@gmail.com>
2019-08-07 16:55:16 +0530

    test tokens

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,8 +1,11 @@
 var Finocial = artifacts.require('./Finocial.sol');
+var FinocialLoan = artifacts.require('./FinocialLoan.sol');
 var StandardToken = artifacts.require('./StandardToken.sol');
 module.exports = async function(deployer, network, accounts) {
 
   deployer.deploy(Finocial);
+  deployer.deploy(FinocialLoan);
+
 
   /**
   * below deployment should be only for Development
