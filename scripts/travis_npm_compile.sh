fd58accb0b4c5b6e3858e0f37e50808d252202c7
Saransh Sharma <marktwain@gmx.us>
2019-09-01 23:48:12 +0530

    Sudo ADded

--- scripts/travis_npm_compile.sh
+++ scripts/travis_npm_compile.sh
@@ -3,7 +3,7 @@
 # if we are on master and it has a tag, prepare build artifacts for npm package
 if [[ -v TRAVIS_TAG ]]
 then
-  npm install
-  npm install -g truffle
+  sudo npm install
+  sudo npm install -g truffle
   truffle compile --all
 fi
