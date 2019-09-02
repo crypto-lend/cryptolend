c9f94c75a792a77f442753cff0c51334fae52f7c
livingrock7 <chiragtitiya6893@gmail.com>
2019-09-02 22:54:12 +0530

    Update LoanContract.sol

--- contracts/LoanContract.sol
+++ contracts/LoanContract.sol
@@ -23,8 +23,8 @@
 */
 pragma solidity ^0.5.0;
 
-import "github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
-import "github.com/OpenZeppelin/openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+import "openzeppelin-solidity/contracts/lifecycle/Pausable.sol";
 import "./LoanMath.sol";
 
 contract LoanContract {
